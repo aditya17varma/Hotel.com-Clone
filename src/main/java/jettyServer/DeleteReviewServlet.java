4a659886b4dde71f1ce44b63e6a3139d91958a6d
aditya17varma <69626061+aditya17varma@users.noreply.github.com>
2022-11-28 10:32:36 -0800

    edit and delete

--- src/main/java/jettyServer/DeleteReviewServlet.java
+++ src/main/java/jettyServer/DeleteReviewServlet.java
@@ -1,19 +1,34 @@
 package jettyServer;
 
 import com.google.gson.JsonObject;
+import hotelapp.Hotel;
 import hotelapp.HotelSearch;
 import hotelapp.Review;
 import org.apache.commons.text.StringEscapeUtils;
+import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
 
 public class DeleteReviewServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        response.sendRedirect("/login");
+    }
+
+
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -25,29 +40,40 @@ public class DeleteReviewServlet extends HttpServlet {
         String sessionName = (String) session.getAttribute("username");
         sessionName = StringEscapeUtils.escapeHtml4(sessionName);
 
-        JsonObject infoJSON = (JsonObject) session.getAttribute("infoJSON");
-        JsonObject hotelJSON = infoJSON.get("hotelData").getAsJsonObject();
-
-        String hotelId = request.getParameter("hotelId");
-        hotelId = StringEscapeUtils.escapeHtml4(hotelId);
-        String reviewId = request.getParameter("reviewId");
-        reviewId = StringEscapeUtils.escapeHtml4(reviewId);
-
-
+        StringWriter writer = new StringWriter();
         VelocityEngine ve = (VelocityEngine) request.getServletContext().getAttribute("templateEngine");
         VelocityContext context = new VelocityContext();
+        Template template;
 
-        if (hotelId != null && reviewId != null){
-            Review tempR = hs.findReview(hotelId, reviewId);
-            if (tempR != null){
-                hs.deleteReview(hotelId, reviewId);
+        if (sessionName != null){
+            String hotelId = request.getParameter("hotelId");
+            hotelId = StringEscapeUtils.escapeHtml4(hotelId);
+            String reviewId = request.getParameter("reviewId");
+            reviewId = StringEscapeUtils.escapeHtml4(reviewId);
+
+            if (hotelId != null && reviewId != null){
+                Review tempR = hs.findReview(hotelId, reviewId);
+                if (tempR != null){
+                    hs.deleteReview(hotelId, reviewId);
+                    response.sendRedirect("/hotelInfoReview?hotelId=" + hotelId);
+                }
+            }
+            else {
+                System.out.println("cannot delete review");
                 response.sendRedirect("/hotelInfoReview?hotelId=" + hotelId);
             }
         }
         else {
-            System.out.println("cannot delete review");
-            response.sendRedirect("/hotelInfoReview?hotelId=" + hotelId);
+            //redirect to login or register
+            template = ve.getTemplate("templates/noLoginTemplate.html");
+            template.merge(context, writer);
         }
+
+        PrintWriter out = response.getWriter();
+
+        out.println(writer.toString());
+
+
     }
 
 
