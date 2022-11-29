fa5c058007cd72bafcf04dba5070b51679ae12bc
aditya17varma <69626061+aditya17varma@users.noreply.github.com>
2022-11-28 18:27:43 -0800

    cleaning up

--- src/main/java/jettyServer/AddReviewServlet.java
+++ src/main/java/jettyServer/AddReviewServlet.java
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
@@ -123,7 +122,7 @@ public class AddReviewServlet extends HttpServlet {
         }
 
         else if (hotelId != null){
-            response.sendRedirect("/addReview?hotelId=" + hotelId + "&hotelName=" + hotelName + "$username=" + username);
+            response.sendRedirect("/addReview?hotelId=" + hotelId + "&hotelName=" + hotelName + "&username=" + username);
         }
         else {
             System.out.println("cannot add review");
